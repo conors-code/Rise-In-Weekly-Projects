32bbebfb28768dedce7d8127f6357247030b853e
conors-code <conor_darcy@hotmail.com>
2023-09-27 19:28:27 +0100

    updated from bool to days names containing "n"

--- week3/src/main.rs
+++ week3/src/main.rs
@@ -1,12 +1,16 @@
-//use std::fmt;
-
 //Create a test collection and a filter condition instance.
 //Filter the collection and print the result.
+//Pick days of the week containing the letter 'n'.
 fn main() {
-    let items_to_check: Vec<bool> = vec![true, true, false, true, false];
-    let my_filter_cond :FilterCondition = FilterCondition { pickme: true };
+    let items_to_check: Vec<&str> = vec![
+        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+    ];
 
-    let my_filtered_items: Vec<bool> = custom_filter(
+    let my_filter_cond :FilterCondition = FilterCondition { 
+        pickme: String::from("n") 
+    };
+
+    let my_filtered_items: Vec<&str> = custom_filter(
       my_filter_cond, items_to_check
     );
     println!("My filtered items are {:?}", my_filtered_items);
@@ -14,20 +18,20 @@ fn main() {
 }
 //define a rust struct called FilterCondition
 struct FilterCondition {
-    pickme : bool,
+    pickme : String
 }
 
 //is_match method on the FilterCondition takes a reference to an
 //item of the same type as the filter condition and returns a boolean 
 // indicating whether the item matches the condition or not.
 impl FilterCondition {
-    fn is_match(&self, check_me: bool) -> bool {
-        self.pickme == check_me
+    fn is_match(&self, check_me: &str) -> bool {
+        check_me.contains(&self.pickme)
     }
 }
 //custom_filter function takes a collection (e.g., a vector) and a 
 //FilterCondition reference, iterates over the collection and returns a
 //new collection of elements matching the condition.
-fn custom_filter(condition: FilterCondition, items: Vec<bool>) -> Vec<bool> {
+fn custom_filter(condition: FilterCondition, items: Vec<&str>) -> Vec<&str> {
     items.into_iter().filter(|&item| condition.is_match(item)).collect()
 }
